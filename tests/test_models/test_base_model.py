f0fabdf88fb056e7daf60830444c02ba406f0ea1
DovaZk <silva.joel.08@gmail.com>
2021-07-01 00:36:31 +0000

    Test Model add

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -6,6 +6,7 @@ import models
 from models.base_model import BaseModel
 import os
 import unittest
+import datetime
 
 
 class Test_Base_Model(unittest.TestCase):
@@ -13,3 +14,12 @@ class Test_Base_Model(unittest.TestCase):
 	Unittesting class
     """
 
+    def test_datetime(self):
+        """
+        Checks for datetime attributes
+        """
+        #Test if two instnace has diferent datetime
+        my_amenity = Amenity()
+        my_amenity_2 = Amenity()
+        self.assertNotEqual(my_amenity.created_at, my_amenity_2.created_at)
+        self.assertNotEqual(my_amenity.updated_at, my_amenity_2.updated_at)
