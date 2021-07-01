40425b5391b4f8ace00f39ae63b7ed5900212e27
giosermon <2172@holbertonschool.com>
2021-06-30 23:46:54 -0500

    fix pep8

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -28,6 +28,7 @@ class FileStorage:
         """ sets in __objects the obj with key <obj class name>.id """
         key = "{}.{}".format(type(obj).__name__, obj.id)
         self.__objects[key] = obj
+
     def save(self):
             """ serializes __objects to the JSON file """
             empty_dict = {}
