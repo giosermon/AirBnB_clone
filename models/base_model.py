4c30ba8b2345ae6e706c610c3adf8e1298148a2d
DovaZk <silva.joel.08@gmail.com>
2021-07-01 00:10:49 +0000

    fix base_model

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-""" Base Model file """
-
-
+"""
+Module for BaseModel class
+"""
 import uuid
 from datetime import datetime
 import models
@@ -10,10 +10,26 @@ format_iso = "%Y-%m-%dT%H:%M:%S.%f"
 
 
 class BaseModel():
-    """ Defines all common attributes/methods for other classes """
-
+    """
+    BaseModel class
+    Defines all common attributes/methods for other classes:
+    Instance attributes:
+        Public:
+            - id
+            - created_at
+            - updated_at
+    Instance methods:
+        Public:
+            - save(self)
+            - to_dict(self)
+    Special method:
+        - __init__(self)
+        - __str__(self)
+    """
     def __init__(self, *args, **kwargs):
-        '''Class constructor'''
+        """
+	Initializes an instance of BaseModel class
+        """
         if kwargs is None or len(kwargs) == 0:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -30,24 +46,24 @@ class BaseModel():
                 elif key is not "__class__":
                     self.__dict__[key] = value
     def __str__(self):
-        """ Method that override the str method and returns a specific string
-            Args: self
-            Return: [<class name>] (<self.id>) <self.__dict__>
+        """
+	Returns a string representation of an instance
         """
         return "[{}] ({}) {}".format(self.__class__.__name__,
                                      self.id, self.__dict__)
 
     def save(self):
         """
-            updates the public instance attribute updated_at
-            with the current datetime
+	Updates the public instance update_at with the current time
         """
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        '''Returns a dictionary containing all keys/values
-        of __dict__ of the instance '''
+        """
+	Returns a dictionary containing all keys/values
+        of __dict__ of the instance
+        """
         my_dic = dict(self.__dict__)
         # make variable my_dict
         my_dic["__class__"] = self.__class__.__name__
