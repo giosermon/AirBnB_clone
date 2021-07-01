ef4467b04b3f1260be42fa1147344e6eff3088cd
DovaZk <silva.joel.08@gmail.com>
2021-07-01 03:53:14 +0000

    Test File Storage v.1

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -7,9 +7,20 @@ from models.engine import file_storage
 from models.engine.file_storage import FileStorage
 import os
 import unittest
+import datetime
 
 
 class Test_File_Storage(unittest.TestCase):
     """
 	Unittesting class
     """
+
+    def test_datetime(self):
+        """
+        Checks for datetime attributes
+        """
+        #Test if two instnace has diferent datetime
+        my_amenity = Amenity()
+        my_amenity_2 = Amenity()
+        self.assertNotEqual(my_amenity.created_at, my_amenity_2.created_at)
+        self.assertNotEqual(my_amenity.updated_at, my_amenity_2.updated_at)
