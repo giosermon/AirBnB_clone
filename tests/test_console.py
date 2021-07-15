2772ba63a20d47594a62137db33cf46b40abbebf
giosermon <2172@holbertonschool.com>
2021-07-15 15:08:25 -0500

    update

--- tests/test_console.py
+++ tests/test_console.py
@@ -39,88 +39,88 @@ class TestConsole(unittest.TestCase):
                 'State',
                 'User']
 
-@classmethod
-def setUpClass(cls):
-        """Set up for every test
-        """
-        FileStorage._FileStorage__file_path = TestConsole.jsfile_test
+    @classmethod
+    def setUpClass(cls):
+            """Set up for every test
+            """
+            FileStorage._FileStorage__file_path = TestConsole.jsfile_test
 
-def tearDown(self):
-        """ tear down method for every test
-        """
-        if os.path.isfile(TestConsole.jsfile_test):
-            os.remove(TestConsole.jsfile_test)
+    def tearDown(self):
+            """ tear down method for every test
+            """
+            if os.path.isfile(TestConsole.jsfile_test):
+                os.remove(TestConsole.jsfile_test)
 
-def test_console_pep8_conformance(self):
-        """The Console code is PEP8 conformant?
-        """
-        style = pep8.StyleGuide(quiet=True)
-        result = style.check_files(['console.py'])
-        self.assertEqual(result.total_errors, 0)
+    def test_console_pep8_conformance(self):
+            """The Console code is PEP8 conformant?
+            """
+            style = pep8.StyleGuide(quiet=True)
+            result = style.check_files(['console.py'])
+            self.assertEqual(result.total_errors, 0)
 
-def test_console_test_pep8_conformance(self):
-        """ The Console Test code is PEP8 conformant?
-        """
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(
-            ['tests/test_console.py'])
-        self.assertEqual(result.total_errors, 0)
+    def test_console_test_pep8_conformance(self):
+            """ The Console Test code is PEP8 conformant?
+            """
+            pep8style = pep8.StyleGuide(quiet=True)
+            result = pep8style.check_files(
+                ['tests/test_console.py'])
+            self.assertEqual(result.total_errors, 0)
 
-def test_console_documented(self):
-        """Console has some documentation?
-        """
-        self.assertTrue
-        self.assertTrue
-        (len(HBNBCommand.__doc__) >= 1)
+    def test_console_documented(self):
+            """Console has some documentation?
+            """
+            self.assertTrue
+            self.assertTrue
+            (len(HBNBCommand.__doc__) >= 1)
 
-def test_create_missing_class(self):
-        correct = "** class name missing **"
-        with patch("sys.stdout", new=StringIO()) as output:
-            self.assertFalse(HBNBCommand().onecmd("create"))
-            self.assertEqual(correct, output.getvalue().strip())
+    def test_create_missing_class(self):
+            correct = "** class name missing **"
+            with patch("sys.stdout", new=StringIO()) as output:
+                self.assertFalse(HBNBCommand().onecmd("create"))
+                self.assertEqual(correct, output.getvalue().strip())
 
-def test_create_invalid_class(self):
-        correct = "** class doesn't exist **"
-        with patch("sys.stdout", new=StringIO()) as output:
-            self.assertFalse(HBNBCommand().onecmd("create MyModel"))
-            self.assertEqual(correct, output.getvalue().strip())
+    def test_create_invalid_class(self):
+            correct = "** class doesn't exist **"
+            with patch("sys.stdout", new=StringIO()) as output:
+                self.assertFalse(HBNBCommand().onecmd("create MyModel"))
+                self.assertEqual(correct, output.getvalue().strip())
 
-def test_create_object(self):
-        with patch("sys.stdout", new=StringIO()) as output:
-            self.assertFalse(HBNBCommand().onecmd("create BaseModel"))
-            self.assertLess(0, len(output.getvalue().strip()))
-            testKey = "BaseModel.{}".format(output.getvalue().strip())
-            self.assertIn(testKey, storage.all().keys())
-        with patch("sys.stdout", new=StringIO()) as output:
-            self.assertFalse(HBNBCommand().onecmd("create User"))
-            self.assertLess(0, len(output.getvalue().strip()))
-            testKey = "User.{}".format(output.getvalue().strip())
-            self.assertIn(testKey, storage.all().keys())
-        with patch("sys.stdout", new=StringIO()) as output:
-            self.assertFalse(HBNBCommand().onecmd("create State"))
-            self.assertLess(0, len(output.getvalue().strip()))
-            testKey = "State.{}".format(output.getvalue().strip())
-            self.assertIn(testKey, storage.all().keys())
-        with patch("sys.stdout", new=StringIO()) as output:
-            self.assertFalse(HBNBCommand().onecmd("create City"))
-            self.assertLess(0, len(output.getvalue().strip()))
-            testKey = "City.{}".format(output.getvalue().strip())
-            self.assertIn(testKey, storage.all().keys())
-        with patch("sys.stdout", new=StringIO()) as output:
-            self.assertFalse(HBNBCommand().onecmd("create Amenity"))
-            self.assertLess(0, len(output.getvalue().strip()))
-            testKey = "Amenity.{}".format(output.getvalue().strip())
-            self.assertIn(testKey, storage.all().keys())
-        with patch("sys.stdout", new=StringIO()) as output:
-            self.assertFalse(HBNBCommand().onecmd("create Place"))
-            self.assertLess(0, len(output.getvalue().strip()))
-            testKey = "Place.{}".format(output.getvalue().strip())
-            self.assertIn(testKey, storage.all().keys())
-        with patch("sys.stdout", new=StringIO()) as output:
-            self.assertFalse(HBNBCommand().onecmd("create Review"))
-            self.assertLess(0, len(output.getvalue().strip()))
-            testKey = "Review.{}".format(output.getvalue().strip())
-            self.assertIn(testKey, storage.all().keys())
+    def test_create_object(self):
+            with patch("sys.stdout", new=StringIO()) as output:
+                self.assertFalse(HBNBCommand().onecmd("create BaseModel"))
+                self.assertLess(0, len(output.getvalue().strip()))
+                testKey = "BaseModel.{}".format(output.getvalue().strip())
+                self.assertIn(testKey, storage.all().keys())
+            with patch("sys.stdout", new=StringIO()) as output:
+                self.assertFalse(HBNBCommand().onecmd("create User"))
+                self.assertLess(0, len(output.getvalue().strip()))
+                testKey = "User.{}".format(output.getvalue().strip())
+                self.assertIn(testKey, storage.all().keys())
+            with patch("sys.stdout", new=StringIO()) as output:
+                self.assertFalse(HBNBCommand().onecmd("create State"))
+                self.assertLess(0, len(output.getvalue().strip()))
+                testKey = "State.{}".format(output.getvalue().strip())
+                self.assertIn(testKey, storage.all().keys())
+            with patch("sys.stdout", new=StringIO()) as output:
+                self.assertFalse(HBNBCommand().onecmd("create City"))
+                self.assertLess(0, len(output.getvalue().strip()))
+                testKey = "City.{}".format(output.getvalue().strip())
+                self.assertIn(testKey, storage.all().keys())
+            with patch("sys.stdout", new=StringIO()) as output:
+                self.assertFalse(HBNBCommand().onecmd("create Amenity"))
+                self.assertLess(0, len(output.getvalue().strip()))
+                testKey = "Amenity.{}".format(output.getvalue().strip())
+                self.assertIn(testKey, storage.all().keys())
+            with patch("sys.stdout", new=StringIO()) as output:
+                self.assertFalse(HBNBCommand().onecmd("create Place"))
+                self.assertLess(0, len(output.getvalue().strip()))
+                testKey = "Place.{}".format(output.getvalue().strip())
+                self.assertIn(testKey, storage.all().keys())
+            with patch("sys.stdout", new=StringIO()) as output:
+                self.assertFalse(HBNBCommand().onecmd("create Review"))
+                self.assertLess(0, len(output.getvalue().strip()))
+                testKey = "Review.{}".format(output.getvalue().strip())
+                self.assertIn(testKey, storage.all().keys())
 
 
 if __name__ == "__main__":
